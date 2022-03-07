b664792fcdf05951ad143d133b8da791881b450a
mgalus <mgalus@dreamlab.pl>
2022-03-07 18:10:57 +0100

    1201923522904867

--- views/article/view.php
+++ views/article/view.php
@@ -8,34 +8,23 @@ $this->registerLinkTag(['rel' => 'canonical', 'href' => \yii\helpers\Url::canoni
 $this->params['breadcrumbs'][] = $model->title;
 
 ?>
-<?= $this->render('/common/breadcrumps') ?>
 
-<section class="Section News News--single animatedParent">
-    <div class="container fadeIn animated">
-        <div>
-            <div class="Card">
-                <div class="Card__header__wrapper">
-                    <? if ($model->file && $model->file->isImage()): ?>
-                        <img class="Card__image" src="<?= $model->file->getImageSrc() ?>"/>
-                    <? endif ?>
-                    <div class="Card__header-block">
-                        <div class="Projects__card__date">
-                            <img
-                                    src="/svg/kalendarz.svg"
-                                    class="Projects__card__date__ico"
-                                    alt=""
-                            />
-                            <?= date('d.m.Y', strtotime($model->created_on)) ?>
-                        </div>
-                        <h1><?= $model->title ?></h1>
+<section class="service-wrapper company-wrapper login">
+    <div class="container">
+        <div class="breadcrumb">
+            <a href="/"> meetfaces </a>
+            <span><?= $model->title ?></span>
+        </div>
+
+        <div class="service single-company">
+            <div class="flex">
+                <div>
+                    <h1><?= $model->title ?></h1>
+                    <div class="content">
+                        <?= $model->content ?>
                     </div>
-                </div>
-                <div class="Card__body">
-                    <?= $model->content ?>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-<?= $this->render('/common/news') ?>
--- views/site/index/map.php
+++ views/site/index/map.php
@@ -1,10 +1,6 @@
 <?
 
-use app\models\mgcms\db\Project;
-
-$projects = Project::find()->all();
-
-
+/* @var $this yii\web\View */
 ?>
 <section class="world-map-container">
     <div class="container">
@@ -14,29 +10,56 @@ $projects = Project::find()->all();
             firmy i dowiedz się, jakie produkty dostępne są w Twoim regionie.
         </p>
         <div class="world-map">
-            <img src="./svg/mapa_swiata.svg" alt="" />
+            <div class="map-container">
+                <img
+                        class="continent continent__1"
+                        src="/svg/mapa_kontynent1.svg"
+                        alt=""
+                />
+                <img
+                        class="continent continent__2"
+                        src="/svg/mapa_kontynent2.svg"
+                        alt=""
+                />
+                <img
+                        class="continent continent__3"
+                        src="/svg/mapa_kontynent3.svg"
+                        alt=""
+                />
+                <img
+                        class="continent continent__4"
+                        src="/svg/mapa_kontynent4.svg"
+                        alt=""
+                />
+                <img
+                        class="continent continent__5"
+                        src="/svg/mapa_kontynent5.svg"
+                        alt=""
+                />
+            </div>
+
             <div class="world-map__countries">
                 <a class="world-map__country" href="">
-                    <img src="./img/flaga_kanada.png" alt="" />
+                    <img src="/img/flaga_kanada.png" alt="" />
                     <div>Kanada</div>
                 </a>
                 <a class="world-map__country" href="">
-                    <img src="./img/flaga_kuba.png" alt="" />
-                    <div>Kuba</div>
+                    <img src="/img/flaga_kuba.png" alt="" />
+                    <div>Kuna</div>
                     <a class="world-map__country world-map__country--active" href="">
-                        <img src="./img/flaga_meksyk.jpeg" alt="" />
+                        <img src="/img/flaga_meksyk.jpeg" alt="" />
                         <div>Meksyk</div>
                     </a>
                     <a class="world-map__country" href="">
-                        <img src="./img/flaga_panama.png" alt="" />
+                        <img src="/img/flaga_panama.png" alt="" />
                         <div>Panama</div>
                     </a>
                     <a class="world-map__country" href="">
-                        <img src="./img/flaga_usa.svg" alt="" />
+                        <img src="/img/flaga_usa.svg" alt="" />
                         <div>Stany Zjednoczone</div>
                     </a>
                     <a class="world-map__country" href="">
-                        <img src="./img/flaga_kanada.png" alt="" />
+                        <img src="/img/flaga_kanada.png" alt="" />
                         <div>Kanada</div>
                     </a>
                 </a>
