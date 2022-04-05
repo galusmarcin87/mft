c56a6125892558a5d8e3cc91420d8843b8939d9c
mgalus <galusmarcin87@gmail.com>
2022-04-05 21:28:16 +0200

    1201894606711378

--- views/site/index/map.php
+++ views/site/index/map.php
@@ -1,6 +1,16 @@
 <?
 
 /* @var $this yii\web\View */
+
+use app\components\mgcms\MgHelpers;
+
+$continents = [
+    0 => MgHelpers::getSettingsArray('kontynent - ameryka pólnocna'),
+    1 => MgHelpers::getSettingsArray('kontynent - ameryka południowa'),
+    2 => MgHelpers::getSettingsArray('kontynent - euroazja'),
+    3 => MgHelpers::getSettingsArray('kontynent - afryka'),
+    4 => MgHelpers::getSettingsArray('kontynent - australia'),
+]
 ?>
 <section class="world-map-container">
     <div class="container">
@@ -15,55 +25,53 @@
                         class="continent continent__1"
                         src="/svg/mapa_kontynent1.svg"
                         alt=""
+                        onclick="showContinent(0)"
                 />
                 <img
                         class="continent continent__2"
                         src="/svg/mapa_kontynent2.svg"
                         alt=""
+                        onclick="showContinent(1)"
                 />
                 <img
                         class="continent continent__3"
                         src="/svg/mapa_kontynent3.svg"
                         alt=""
+                        onclick="showContinent(2)"
                 />
                 <img
                         class="continent continent__4"
                         src="/svg/mapa_kontynent4.svg"
                         alt=""
+                        onclick="showContinent(3)"
                 />
                 <img
                         class="continent continent__5"
                         src="/svg/mapa_kontynent5.svg"
                         alt=""
+                        onclick="showContinent(4)"
                 />
             </div>
 
-            <div class="world-map__countries">
-                <a class="world-map__country" href="">
-                    <img src="/img/flaga_kanada.png" alt="" />
-                    <div>Kanada</div>
-                </a>
-                <a class="world-map__country" href="">
-                    <img src="/img/flaga_kuba.png" alt="" />
-                    <div>Kuna</div>
-                    <a class="world-map__country world-map__country--active" href="">
-                        <img src="/img/flaga_meksyk.jpeg" alt="" />
-                        <div>Meksyk</div>
-                    </a>
-                    <a class="world-map__country" href="">
-                        <img src="/img/flaga_panama.png" alt="" />
-                        <div>Panama</div>
-                    </a>
-                    <a class="world-map__country" href="">
-                        <img src="/img/flaga_usa.svg" alt="" />
-                        <div>Stany Zjednoczone</div>
-                    </a>
-                    <a class="world-map__country" href="">
-                        <img src="/img/flaga_kanada.png" alt="" />
-                        <div>Kanada</div>
-                    </a>
-                </a>
-            </div>
+            <? foreach ($continents as $continentIndex => $countries): ?>
+                <div class="world-map__countries" id="country_<?= $continentIndex ?>" style="display: none">
+                    <? foreach ($countries as $country): ?>
+                        <a class="world-map__country"
+                           href="<?= \yii\helpers\Url::to(['/company/index', 'country' => $country]) ?>">
+                            <img src="<?= MgHelpers::getSetting('flaga - ' . $country) ?>" alt=""/>
+                            <div><?= Yii::t('db', $country) ?></div>
+                        </a>
+                    <? endforeach ?>
+                </div>
+            <? endforeach ?>
+
         </div>
     </div>
 </section>
+
+<script>
+  function showContinent (index) {
+    $('.world-map__countries').hide();
+    $('#country_' + index).show();
+  }
+</script>
