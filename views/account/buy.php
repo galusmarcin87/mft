bf9f92f1840142988a20ff5916ab54172c7d9c7e
mgalus <galusmarcin87@gmail.com>
2022-08-01 22:22:19 +0200

    aaa

--- views/account/buy.php
+++ views/account/buy.php
@@ -15,32 +15,40 @@ use kartik\icons\Icon;
 <script src="https://js.stripe.com/v3/"></script>
 
 
-
 <section class="companies-wrapper companies-wrapper--dashboard">
     <div class="container">
-        <form id="payment-form" data-secret="<?= $clientSecret ?>">
-            <div id="payment-element">
-                <!-- Elements will create form elements here -->
-            </div>
+        <div class="row">
+            <div class="col-md-8 offset-2">
+                <form id="payment-form" data-secret="<?= $clientSecret ?>">
+                    <div id="payment-element">
+                        <!-- Elements will create form elements here -->
+                    </div>
 
-            <button id="submit">Submit</button>
+                    <div class="text-center mt-2    ">
+                        <button id="submit" class="btn btn-primary"><?= Yii::t('db', 'Submit') ?></button>
+                    </div>
 
-            <div id="error-message">
-                <!-- Display error message to your customers here -->
+
+                    <div id="error-message">
+                        <!-- Display error message to your customers here -->
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </section>
 
 
 <script>
-    const stripe = Stripe('<?= MgHelpers::getSetting('stripe api publishable key', false, 'pk_test_csucV2ibOptYbNVLkjioXWb8004lKvkQVf');?>',{
-        stripeAccount: '<?= $stripeAccount?>',
-    });
+    const stripe = Stripe(
+      '<?= MgHelpers::getSetting('stripe api publishable key', false, 'pk_test_csucV2ibOptYbNVLkjioXWb8004lKvkQVf');?>',
+      {
+          stripeAccount: '<?= $stripeAccount?>',
+      });
     const options = {
         clientSecret: '<?= $clientSecret ?>',
         // Fully customizable with appearance API.
-        appearance: {/*...*/},
+        appearance: {/*...*/ },
     };
 
     // Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in step 2
@@ -55,7 +63,7 @@ use kartik\icons\Icon;
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
 
-        const {error} = await stripe.confirmPayment({
+        const { error } = await stripe.confirmPayment({
             //`Elements` instance that was used to create the Payment Element
             elements,
             confirmParams: {
@@ -66,7 +74,8 @@ use kartik\icons\Icon;
         if (error) {
             const messageContainer = document.querySelector('#error-message');
             messageContainer.textContent = error.message;
-        } else {
+        }
+        else {
         }
     });
 
--- views/account/buyAfter.php
+++ views/account/buyAfter.php
@@ -13,8 +13,16 @@ use yii\bootstrap\ActiveForm;
 ?>
 <section class="companies-wrapper companies-wrapper--dashboard">
     <div class="container">
-        <h1><?= Yii::t('db', 'Thank you for buying') ?></h1>
-        <div><?= MgHelpers::getSetting('buy after ' . $type . ' ' . Yii::$app->language, true, 'buy after ' . $type) ?></div>
-        <a class="btn btn--primary" href="<?= Url::to(['account/rate','hash' => MgHelpers::encrypt($payment->id)])?>"><?= Yii::t('db', 'Rate the purchase') ?></a>
+        <div class="row">
+            <div class="col-md-8 offset-2">
+                <h1><?= Yii::t('db', 'Thank you for buying') ?></h1>
+                <div><?= MgHelpers::getSetting('buy after ' . $type . ' ' . Yii::$app->language, true, 'buy after ' . $type) ?></div>
+                <div class="text-center mt-2    ">
+                    <a class="btn btn--primary"
+                       href="<?= Url::to(['account/rate', 'hash' => MgHelpers::encrypt($payment->id)]) ?>">
+                        <?= Yii::t('db', 'Rate the purchase') ?></a>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
