dbe470bea372e49b2c05a4d2a492f73d96b8c391
mgalus <galusmarcin87@gmail.com>
2022-12-23 23:20:29 +0100

    1202505551784630

--- widgets/AdWidget.php
+++ widgets/AdWidget.php
@@ -14,7 +14,13 @@ class AdWidget extends \yii\bootstrap\Widget
     public function run()
     {
         $ip = Yii::$app->request->userIP;
+        echo '<pre>';
+        echo var_dump($ip);
+        echo '</pre>';
         $country = Yii::$app->ip2location->getCountryCode($ip);
+        echo '<pre>';
+        echo var_dump($country);
+        echo '</pre>';
         $ads = Ad::find()->where(['country' => $country, 'status' => Ad::STATUS_APPROVED])->andWhere(['>', 'date_to', date('Y-m-d')])->all();
         return $this->render('adWidget', ['ads' => $ads]);
     }
