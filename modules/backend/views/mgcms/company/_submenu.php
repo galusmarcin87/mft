6fc99377071ed906a5d7ab45fcbde35f64b5d9a2
galusmarcin87 <galusmarcin87@gmail.com>
2023-09-08 20:53:40 +0200

    1205411262506930

--- modules/backend/views/mgcms/company/_submenu.php
+++ modules/backend/views/mgcms/company/_submenu.php
@@ -7,7 +7,7 @@ $currentUserId = Yii::$app->getRequest()->getQueryParam('CompanySearch') ? Yii::
     <?php foreach ($items as $item): ?>
         <li class="dd-item" data-id="<?php echo $item->id ?>">
             <div class="dd-handle <?= $currentUserId == $item->id ? 'active' : '' ?>"
-                 data-user-id="<?= $item->id ?>"><?= $item ?> <?= Yii::t('db', $item->role) ?></div>
+                 data-user-id="<?= $item->id ?>"><?= $item ?> - <?= Yii::t('db', $item->role) ?></div>
             <?php if (!empty($item->children)): ?>
                 <?php echo $this->render('_submenu', array('items' => $item->children)); ?>
             <?php endif ?>
