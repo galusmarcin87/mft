81c43cecdedf950d55d0240577163186c278cbe9
galusmarcin87 <galusmarcin87@gmail.com>
2023-09-13 21:43:50 +0200

    1205486351150961 mzielinska

--- views/company/view_info.php
+++ views/company/view_info.php
@@ -21,79 +21,78 @@ $this->title = $model->name;
     <div class="container">
         <?= $this->render('view/_breadcrumbs', ['model' => $model]) ?>
 
-        <div class="service single-company">
-            <?= $this->render('view/_logo', ['model' => $model]) ?>
-            <div class="single-company__content ">
-                <?= $this->render('view/_top', ['model' => $model]) ?>
-                <?= $this->render('view/_buttons', ['model' => $model]) ?>
+<div class="service single-company">
+    <?= $this->render('view/_logo', ['model' => $model]) ?>
+    <div class="single-company__content ">
+        <?= $this->render('view/_top', ['model' => $model]) ?>
+        <?= $this->render('view/_buttons', ['model' => $model]) ?>
 
-                <h3><?= Yii::t('db', 'General information') ?></h3>
-                <div class="description">
-                    <?= $model->description ?>
-                </div>
-                <?= $this->render('view/_contactData',['model' => $model]) ?>
-                <?= $this->render('view/_forSaleData',['model' => $model]) ?>
-                <? if ($model->gps_lat && $model->gps_long): ?>
-                    <h3><?= Yii::t('db', 'Localization') ?></h3>
-                    <div id="MAP"></div>
-                <? endif ?>
+        <?php if ($model->description): ?>
+            <h3><?= Yii::t('db', 'General information') ?></h3>
+            <div class="description">
+                <?= $model->description ?>
+            </div><br>
+        <?php endif; ?>
 
-                <? if (count($model->fileRelations)): ?>
-                    <h3><?= Yii::t('db', 'Gallery') ?></h3>
-                    <div class="carousel-wrap">
-                        <div class="owl-carousel owl-theme" id="GALLERY">
-                            <? foreach ($model->fileRelations as $relation): ?>
+        <?= $this->render('view/_contactData', ['model' => $model]) ?>
+        <?= $this->render('view/_forSaleData', ['model' => $model]) ?>
 
-                                <? if ($relation->json == '1' || !$relation->file) continue ?>
+        <?php if ($model->gps_lat && $model->gps_long): ?>
+            <h3><?= Yii::t('db', 'Localization') ?></h3>
+            <div id="MAP"></div>
+        <?php endif; ?>
 
-                                <img
-                                        class="item"
-                                        src="<?= $relation->file->getImageSrc() ?>"
-                                />
-                            <? endforeach ?>
+        <?php $hasGallery = false; ?>
 
-                        </div>
-                    </div>
-                <? endif ?>
-                <h3><?= Yii::t('db', 'Movie about company') ?></h3>
-                <div class="single-company__video">
-                    <a
-                            class="popup-video"
-                            href="<?= $model->video ?>"
-                    >
-                        <img src="<?= $model->video_thumbnail ?>"/>
-                        <img
-                                class="single-company__video__play"
-                                src="/svg/play.svg"
-                                alt=""
-                        />
-                    </a>
-                </div>
-                <div class="flex">
-                    <? if (count($model->fileRelations)): ?>
-                        <div>
-                            <h3><?= Yii::t('db', 'Multimedia') ?></h3>
-                            <div class="carousel-wrap buttons">
-                                <? foreach ($model->fileRelations as $relation): ?>
-                                    <? if ($relation->json != '1' || !$relation->file) continue ?>
-                                    <a href="<?= $relation->file->getLinkUrl() ?>" class="btn btn--primary btn--small">
-                                        <?= $relation->file->origin_name ?>
-                                    </a>
-                                <? endforeach ?>
-
-                            </div>
-                        </div>
-                    <? endif ?>
-                    <?= $this->render('/common/_socialShare', [
-                        'title' => $model->name,
-                        'description' => $model->description,
-                        'image' => $model->thumbnail && $model->thumbnail->isImage() ? $model->thumbnail->getImageSrc(240, 0) : false,
-                    ]) ?>
+        <?php if (count($model->fileRelations) > 0): ?>
+            <?php $hasGallery = true; ?>
+            <h3><?= Yii::t('db', 'Gallery') ?></h3>
+            <div class="carousel-wrap">
+                <div class="owl-carousel owl-theme" id="GALLERY">
+                    <?php foreach ($model->fileRelations as $relation): ?>
+                        <?php if ($relation->json == '1' || !$relation->file) continue; ?>
+                        <img class="item" src="<?= $relation->file->getImageSrc() ?>"/>
+                    <?php endforeach; ?>
                 </div>
             </div>
+        <?php endif; ?>
+
+        <?php if ($model->video): ?>
+            <h3><?= Yii::t('db', 'Movie about company') ?></h3>
+            <div class="single-company__video">
+                <a class="popup-video" href="<?= $model->video ?>">
+                    <img src="<?= $model->video_thumbnail ?>"/>
+                    <img class="single-company__video__play" src="/svg/play.svg" alt=""/>
+                </a>
+            </div>
+        <?php endif; ?>
+
+        <div class="flex">	
+            <?php if ($hasGallery): ?>
+                <div class="carousel-wrap buttons">
+                    <h3><?= Yii::t('db', 'Multimedia') ?></h3>
+                    <?php foreach ($model->fileRelations as $relation): ?>
+                        <?php if ($relation->json != '1' || !$relation->file) continue; ?>
+                        <a href="<?= $relation->file->getLinkUrl() ?>" class="btn btn--primary btn--small">
+                            <?= $relation->file->origin_name ?>
+                        </a>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+
+            <?= $this->render('/common/_socialShare', [
+                'title' => $model->name,
+                'description' => $model->description,
+                'image' => $model->thumbnail && $model->thumbnail->isImage() ? $model->thumbnail->getImageSrc(240, 0) : false,
+            ]) ?>
         </div>
     </div>
+</div>
+
+		
+    </div>
 </section>
+
 <? if ($model->gps_lat && $model->gps_long): ?>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDeGtxbtJfB88Fgff3N_um_SjNBNAROskU"></script>
     <script src="https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/src/markerclusterer.js"></script>
